beee0285c42972c32964ac03277ec6ada54d2a99
Deckard Kwon <deckore@outlook.com>
2023-08-07 11:56:55 +0900

    Added ch8_4_stateful

---
+++ lib/ch8_4_stateful/test.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stateful Widget'),
+        ),
+        body: MyWidget(),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() {
+    return _MyWidgetState();
+  }
+}
+
+class _MyWidgetState extends State<MyWidget> {
+
+  bool enabled = false;
+  String stateText = "Disabled";
+
+  void changeCheck() {
+    setState(() {
+      if (enabled) {
+        stateText = "Disabled";
+        enabled = false;
+      } else {
+        stateText = "Enabled";
+        enabled = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: changeCheck,
+            icon: (enabled ? const Icon(Icons.check_box, size: 20) : const Icon(Icons.check_box_outline_blank, size: 20)),
+            color: Colors.red,                
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 16),
+            child: Text(
+              "${stateText}",
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
