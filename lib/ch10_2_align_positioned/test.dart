24b558891e99103c719c0904dd9d6e152033d807
Deckard Kwon <deckore@outlook.com>
2023-08-11 08:45:35 +0900

    ch10_2_align_positioned

---
+++ lib/ch10_2_align_positioned/test.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Align & Positioned")),
+        body: Stack(
+          children: [
+            Align(
+              alignment: const Alignment(0.0, 0.0),
+              child: Container(
+                width: 150,
+                height: 150,
+                color: Colors.yellow,
+              ),
+            ),
+            Align(
+              alignment: const FractionalOffset(1.0, 0.0),
+              child: Container(
+                width: 150,
+                height: 150,
+                color: Colors.blue,
+              ),
+            ),
+            Positioned(
+              left: 40.0,
+              top: 40.0,
+              child: Container(
+                width: 150,
+                height: 150,
+                color: Colors.pink,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
