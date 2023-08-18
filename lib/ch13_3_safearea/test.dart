358ea9dca75e96129c28937e5e23fb4495510584
Deckore <prodigy9090@gmail.com>
2023-08-19 07:49:31 +0900

    ch13_3_SafeArea

---
+++ lib/ch13_3_safearea/test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  List<Widget> getWidgets() {
+    List<Widget> widgets = [];
+    for (var i = 0; i < 100; i++) {
+      widgets.add(ListTile(title: Text("Hello World Item ${i}")));
+    }
+    return widgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: getWidgets(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
